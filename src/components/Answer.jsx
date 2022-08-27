a14efeb1ea685e364dfeb1d6de29539b01997d69
Farul Ivan <farulivan@gmail.com>
2022-08-27 15:46:24 +0700

    add styling to answeroptions

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -11,23 +11,27 @@ const Answer = (props) => {
         setAnswerOptions(handleShuffle(options))
     },[])
 
-    // console.log(answerOptions)
-
     return ( 
         <section className="answer-option flex flex-row my-3 gap-3 font-Inter font-medium">
             {answerOptions?.map(answer => {
                 return (
                 <section key={answer}>
                     <input
-                        className=""
+                        className="hidden peer"
                         type="radio"
-                        id= {answer}
+                        id={answer}
                         name={props.index}
-                        value= {answer}
+                        value={answer}
                         // checked={formData.employment ===  {answer}}
                         onChange={props.handleChange}
+                        required
                     />
-                    <label htmlFor= {answer}>{answer}</label>
+                    <label 
+                        className="inline-flex font-Inter font-normal text-sm justify-between items-center px-5 py-2 w-full bg-white rounded-2xl border border-sky-900 cursor-pointer peer-checked:bg-sky-200 peer-checked:border-0 hover:bg-gray-100"
+                        htmlFor={answer}
+                    >
+                        {answer}
+                    </label>
                 </section>
                 )
             })}
