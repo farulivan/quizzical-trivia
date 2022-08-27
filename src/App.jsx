072670871082c9e5e55b3398e9e3b0c82c3aa74f
Farul Ivan <farulivan@gmail.com>
2022-08-28 00:12:20 +0700

    fixing media query

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="bg-fixed text-sky-900 bg-[url('/ssscribble.svg')] h-screen flex justify-center items-center font-Inter font-normal">
-        <section className="quiz-container min-h-screen min-w-screen sm:min-w-[30rem] sm:min-h-[30rem] flex flex-col items-center justify-center sm:m-7 p-12 bg-white rounded-3xl bg-[url('/blue-blobs.svg'),_url('/yellow-blobs.svg')] bg-[position:bottom_left,_top_right] bg-no-repeat">
+        <section className="quiz-container min-h-full min-w-full sm:min-w-[30rem] sm:min-h-[30rem] flex flex-col items-center justify-center sm:m-7 p-12 bg-white rounded-3xl bg-[url('/blue-blobs.svg'),_url('/yellow-blobs.svg')] bg-[position:bottom_left,_top_right] bg-no-repeat">
           {isHome ? 
             <Home start={() => setIsHome(false)} /> 
             : <Questions back={() => setIsHome(true)} />
